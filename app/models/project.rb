877cf31fbc87a9dc9799485aa3d4ee3aecda59d4
Marwan Rouissi <rouissi-marwan@hotmail.fr>
2025-05-10 21:15:07 +0200

    added images for projects + style for project's details

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,15 @@
+require 'json'
+
 class Project < ApplicationRecord
+  def self.get_stack_icon(techno)
+    # Load the JSON data from the file
+    file_path = Rails.root.join('lib', 'assets', 'stack_logo.json')
+    file_content = File.read(file_path)
+    stack_logo_data = JSON.parse(file_content)
+
+    # Debugging statement
+    puts "Looking for: #{techno}"
+    puts "Data: #{stack_logo_data.inspect}"
+    stack_logo_data[techno]
+  end
 end
