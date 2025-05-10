6e283e0b17f649de4258ebfa6ba64df46eed0d84
Marwan Rouissi <rouissi-marwan@hotmail.fr>
2025-05-10 15:39:43 +0200

    added model for projects

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_05_02_091027) do
+ActiveRecord::Schema[7.2].define(version: 2025_05_10_132507) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,18 @@ ActiveRecord::Schema[7.2].define(version: 2025_05_02_091027) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "github_url"
+    t.text "technologies", array: true
+    t.text "features", array: true
+    t.text "challenges", array: true
+    t.text "potential_improvements", array: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "solid_cable_messages", force: :cascade do |t|
